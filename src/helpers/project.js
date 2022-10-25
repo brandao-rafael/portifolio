bd50b1cc85beeab89ac8c03c51c921d57f9fbf16
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-25 12:57:48 -0300

    Projects

---
+++ src/helpers/project.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    name: "Trybe Tunes",
+    url: "https://brandao-rafael.github.io/trybe-tunes/",
+    src: "https://github.com/brandao-rafael/trybe-tunes/blob/main/images/print/Captura%20de%20tela%20de%202022-10-25%2010-01-03.png?raw=true",
+  },
+  {
+    name:"Solar System",
+    url: "https://brandao-rafael.github.io/solar-system/",
+    src: "https://github.com/brandao-rafael/solar-system/blob/main/src/images/Captura%20de%20tela%20de%202022-10-25%2010-53-44.png?raw=true",
+  }
+]
+
+export default projects;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
 import ProjectCard from '../components/ProjectCard'
+import projects from '../helpers/project';
 
 export default class Projects extends Component {
   render() {
-    const projects = [
-      {
-        name: "Trybe Tunes",
-        url: "https://brandao-rafael.github.io/trybe-tunes/",
-        src: "https://github.com/brandao-rafael/trybe-tunes/blob/main/images/print/Captura%20de%20tela%20de%202022-10-25%2010-01-03.png?raw=true",
-      },
-      {
-        name:"Solar System",
-        url: "https://brandao-rafael.github.io/solar-system/",
-        src: "https://github.com/brandao-rafael/solar-system/blob/main/src/images/Captura%20de%20tela%20de%202022-10-25%2010-53-44.png?raw=true",
-      }
-    ]
     return (
       <div>
         <Header/>
-        <h1 className="project-title">Projetos desenvolvidos em React</h1>
+        <h1 className="project-title">Projetos desenvolvidos (em construção)</h1>
         <main className="projects-container">
           {projects.map(({name, url, src}) =>(
             <ProjectCard name={ name } url={ url } src={ src }/>
