4b2924492ec121a700c96a0bd61a1bd55291008a
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-10-26 11:59:55 -0300

    add trybe-wallet

--- src/helpers/project.js
+++ src/helpers/project.js
@@ -8,6 +8,11 @@ const projects = [
     name:"Solar System",
     url: "https://brandao-rafael.github.io/solar-system/",
     src: "https://github.com/brandao-rafael/solar-system/blob/main/src/images/Captura%20de%20tela%20de%202022-10-25%2010-53-44.png?raw=true",
+  },
+  {
+    name: "Trybe Wallet",
+    url: "https://brandao-rafael.github.io/trybe-wallet/",
+    src: "https://github.com/brandao-rafael/trybe-wallet/blob/main/imgs/Captura%20de%20tela%20de%202022-10-25%2011-31-56.png?raw=true",
   }
 ]
 
